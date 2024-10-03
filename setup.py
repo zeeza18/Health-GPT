09cab9f641d9a4ac0c1edbda245e7c5517d920c1
zeeza18 <mdazeezulla2001@gmail.com>
2024-10-03 07:39:11 -0500

    template setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Mohammed Azeezulla',
+    author_email= 'mdazeezulla2001@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
